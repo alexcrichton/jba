ae2a7cc626d5d77ff1caffad6ee273c8936507ff
Alex Crichton <alex@alexcrichton.com>
2011-08-10 16:06:23 -0700

    Add a serialization/deserialization test

--- test/public/z80/registers_test.js
+++ test/public/z80/registers_test.js
@@ -15,3 +15,26 @@ test('initialized values', function() {
   equals(reg.pc, 0x100);
   equals(reg.sp, 0xfffe);
 });
+
+test('serialization/deserialization', function() {
+  var reg = new Z80.Registers();
+  reg.a = 0x54; reg.f = 0x84;
+  reg.b = 0x42; reg.c = 0x82;
+  reg.d = 0xf8; reg.e = 0x32;
+  reg.h = 0x21; reg.l = 0x89;
+  reg.pc = 0x3910;
+  reg.sp = 0x4224;
+
+  var io = new JBA.StringIO();
+  reg.serialize(io);
+  io.rewind();
+  var reg2 = new Z80.Registers();
+  reg2.deserialize(io);
+
+  equals(reg2.a, 0x54); equals(reg2.f, 0x84);
+  equals(reg2.b, 0x42); equals(reg2.c, 0x82);
+  equals(reg2.d, 0xf8); equals(reg2.e, 0x32);
+  equals(reg2.h, 0x21); equals(reg2.l, 0x89);
+  equals(reg2.pc, 0x3910);
+  equals(reg2.sp, 0x4224);
+});
